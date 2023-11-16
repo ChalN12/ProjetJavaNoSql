1bdf683deb2a0803feaf200d8acd332a53be47e2
Martin Fouques <martinfouques@gmail.com>
2023-11-16 17:10:26 +0100

    Création html et test html

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    @RequestMapping(value="/acheter", method=RequestMethod.POST)
+    public ModelAndView acheterArticle() {
+        // votre logique d'achat ici
+        return new ModelAndView("confirmation");
+    }
 }
