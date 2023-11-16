589d5c7273b1fad886a62e427909a46d32b74c14
Martin Fouques <martinfouques@gmail.com>
2023-11-16 18:15:05 +0100

    Tentative Exe java html

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static String hello() {
+        return "hello";
+    }
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+}
